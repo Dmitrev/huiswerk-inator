9b185d4761060aea06c10ccf57d713469f8f559f
Dmitri Chebotarev <crashbandie@hotmail.com>
2014-09-11 17:09:22 +0200

    Added test Favicon

--- app/views/templates/default.blade.php
+++ app/views/templates/default.blade.php
@@ -7,7 +7,8 @@
         {{HTML::style('plugins/bootstrap/css/bootstrap.min.css')}}
         <!-- Font awesome -->
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
+        <link rel="apple-touch-icon-precomposed" href="{{URL::asset('favicon.png')}}">
+        <link rel="icon" href="{{URL::asset('favicon.ico')}}" type="image/x-icon" />
         {{HTML::style('css/main.css')}}
                 
         
