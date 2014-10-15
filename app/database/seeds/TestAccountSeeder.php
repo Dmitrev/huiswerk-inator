9adcece58730581b685b88bb70ed176128d25ee3
Dmitri Chebotarev <crashbandie@hotmail.com>
2014-10-15 22:52:51 +0200

    added 20 dummy users for testing

--- app/database/seeds/TestAccountSeeder.php
+++ app/database/seeds/TestAccountSeeder.php
@@ -3,12 +3,23 @@ class TestAccountSeeder extends Seeder {
     public function run()
     {
         User::truncate();
-        
+        /* Admin account */
         $user = new User;
         $user->username = 'test';
         $user->password = Hash::make('test');
         $user->fullname = 'Test Account';
-        $user->group = 0;
+        $user->group = 2;
         $user->save();
+
+
+        for( $i = 0; $i < 20; $i++)
+        {
+          $user = new User;
+          $user->username = 'test'.$i;
+          $user->password = Hash::make('test');
+          $user->fullname = 'Test Account'.$i;
+          $user->group = 0;
+          $user->save();
+        }
     }
 }
