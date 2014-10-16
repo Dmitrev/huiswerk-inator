67b6de3e1a3db3cc85928c696d8d81839bb27e53
Dmitri Chebotarev <crashbandie@hotmail.com>
2014-10-16 10:54:21 +0200

    User now stays logged in if the option is selected during login attempt

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,28 +1,34 @@
 <?php
 
 class AuthController extends BaseController {
-    
+
     public function showLoginForm()
     {
         return View::make('login')
             ->with('title', 'Login op Inholland Huiswerk App');
     }
-    
+
     public function submitLoginCredentials()
     {
-        
+
         $credentials = Input::only(['username', 'password']);
-        
-        if( !Auth::attempt($credentials) )
+        $remember = false;
+
+        if( Input::has('remember_me') && Input::get('remember_me'))
         {
-            
+          $remember = true;
+        }
+
+        if( !Auth::attempt($credentials, $remember) )
+        {
+
             return Redirect::route('login')
                 ->with('error', Config::get('messages.auth.fail') );
         }
-        
+
         return Redirect::route('home');
     }
-    
+
     public function logoutUser()
     {
         Auth::logout();
