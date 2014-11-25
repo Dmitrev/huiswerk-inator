b0288007dd5622d308204340a61a5901fc1dbb93
Dmitri Chebotarev <crashbandie@hotmail.com>
2014-11-25 13:23:01 +0100

    Account page is more mobile friendly now

--- app/views/account.blade.php
+++ app/views/account.blade.php
@@ -3,10 +3,21 @@
 @section('content')
     @include('common.back')
     <h1>Account</h1>
-    <ul>
-        <li><a href="{{URL::route('account-general')}}">Algemeen</a></li>
-        <li><a href="{{URL::route('account-password')}}">Wachtwoord wijzigen</a></li>
-        <li><a href="{{URL::route('account-security')}}">Beveiligingsvraag</a></li>
 
-    </ul>
+    <div class="list-group">
+    <a href="{{URL::route('account-general')}}" class="list-group-item">
+      <h4 class="list-group-item-heading"><i class="fa fa-user"></i> Algemeen</h4>
+      <p class="list-group-item-text">Naam en gebruiksernaam wijzigen</p>
+    </a>
+
+    <a href="{{URL::route('account-password')}}" class="list-group-item">
+      <h4 class="list-group-item-heading"><i class="fa fa-lock"></i> Wachtwoord wijzigen</h4>
+      <p class="list-group-item-text">Wijzig het wachtwoord waar je mee inlogd</p>
+    </a>
+
+    <a href="{{URL::route('account-security')}}" class="list-group-item">
+      <h4 class="list-group-item-heading"><i class="fa fa-key"></i> Beveiligingsvraag</h4>
+      <p class="list-group-item-text">Beveiligingsvraag instellen voor het herstellen van je account</p>
+    </a>
+  </div>
 @stop
