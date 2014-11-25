f678212cfec9b487e4eb620f6f08c984467e4981
Dmitri Chebotarev <crashbandie@hotmail.com>
2014-11-25 22:30:23 +0100

    Added favicons cross-platform

--- app/views/templates/base.blade.php
+++ app/views/templates/base.blade.php
@@ -11,8 +11,22 @@
 
         <!-- Font awesome -->
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link rel="apple-touch-icon-precomposed" href="{{URL::asset('favicon.png')}}">
-        <link rel="icon" href="{{URL::asset('favicon.ico')}}" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="57x57" href="{{URL::asset('images/favicons/apple-touch-icon-57x57.png')}}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{URL::asset('images/favicons/apple-touch-icon-114x114.png')}}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{URL::asset('images/favicons/apple-touch-icon-72x72.png')}}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{URL::asset('images/favicons/apple-touch-icon-144x144.png')}}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{URL::asset('images/favicons/apple-touch-icon-60x60.png')}}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{URL::asset('images/favicons/apple-touch-icon-120x120.png')}}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{URL::asset('images/favicons/apple-touch-icon-76x76.png')}}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{URL::asset('images/favicons/apple-touch-icon-152x152.png')}}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{URL::asset('images/favicons/apple-touch-icon-180x180.png')}}">
+        <link rel="icon" type="image/png" href="{{URL::asset('images/favicons/favicon-192x192.png')}}" sizes="192x192">
+        <link rel="icon" type="image/png" href="{{URL::asset('images/favicons/favicon-160x160.png')}}" sizes="160x160">
+        <link rel="icon" type="image/png" href="{{URL::asset('images/favicons/favicon-96x96.png')}}" sizes="96x96">
+        <link rel="icon" type="image/png" href="{{URL::asset('images/favicons/favicon-16x16.png')}}" sizes="16x16">
+        <link rel="icon" type="image/png" href="{{URL::asset('images/favicons/favicon-32x32.png')}}" sizes="32x32">
+        <meta name="msapplication-TileColor" content="#b91d47">
+        <meta name="msapplication-TileImage" content="{{URL::asset('images/favicons/mstile-144x144.png')}}">
 
         {{HTML::style('css/main.css?v1.0.5')}}
 
